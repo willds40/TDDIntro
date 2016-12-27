4420ea3622894b93bb643350deba415711f5197d
Will Devon-Sand <willds40@gmail.com>
2016-12-27 11:22:40 -0500

    accountBalance changed

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,18 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private int balance;
+    public Account(int balance) {
+       this.balance =balance;
+    }
+
+    public int getBalance(int i) {
+        if (i> balance){
+         balance = i -balance;
+            return balance;
+        }else{
+        balance = balance + i;
+        return balance;
+        }
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -1,24 +1,33 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class AccountTests {
+    private Account account;
+    private  int balance;
+
+    @Before
+    public void setup(){
+    this.account = new Account(100);
+    }
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
+        balance = account.getBalance(50);
+        assertThat(balance, is(150));
     }
-
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-
+        balance = account.getBalance(-50);
+        assertThat(balance, is(50));
     }
-
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
-
+    Account account = new Account(50);
+        balance = account.getBalance(100);
+        assertThat(balance, is(50));
     }
 }
