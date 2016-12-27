6ce8a062f003495b8109ea442735d8c68f1663eb
Will Devon-Sand <willds40@gmail.com>
2016-12-27 10:51:00 -0500

    factorial changes

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,11 +1,19 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
-    public Integer compute(int i)
-    {
+    public Integer compute(int i) {
 
-        System.out.println("This is a facotial");
+            if (i < 0) {
+                throw new IllegalArgumentException();
+            }
+            if (i == 0) {
+                return 1;
+            }
+            if (i > 2) {
+                i = i * 2;
+                return i;
+            }
+            return i;
 
-        return -1;
     }
 }
